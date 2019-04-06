50c0a4384489004709dd116346c011f887913937
Risul Karim <risulkarimx1@gmail.com>
2019-04-07 01:28:33 +0200

    [E2- NoiseGeneration] Added the first basic noise class and generation

---
+++ ProceduralLandmassGeneration/Assets/_Project/Scripts/Noise/MapGenerator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth, mapHeight, noiseScale;
+    public bool autoUpdate = false;
+
+
+    public void GenerateMap()
+    {
+        var noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        MapDisplay disply = FindObjectOfType<MapDisplay>();
+        disply.DrawNoiseMap(noiseMap);
+    }
+}
---
+++ ProceduralLandmassGeneration/Assets/_Project/Scripts/Noise/Noise.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    {
+        float[,] noiseMap = new float[mapHeight,mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                var sampleX = x / scale;
+                var sampleY = y / scale;
+                var perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+        return noiseMap;
+    }
+}
